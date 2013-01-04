92c3baea69ad1919eda047e2421eebd617274ed4
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-01-04 11:36:40 +0100

    DEV: First commit

---
+++ Sudoku/Sudoku/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Alexandre HUMEAU on 04/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
