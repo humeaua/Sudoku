add1173a0648363121a7743dbd47e8cae26bd701
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-01-05 14:22:54 +0100

    DEV: Add Access methods in RegionHolder

--- Sudoku/Sudoku/RegionHolder.cpp
+++ Sudoku/Sudoku/RegionHolder.cpp
@@ -9,5 +9,50 @@
 #include <iostream>
 #include "RegionHolder.h"
 
-RegionHolder::RegionHolder(const Region & sRegion) : sRegion_(sRegion)
+RegionHolder::RegionHolder(const Region & sRegion) : Region(sRegion)
 {}
+
+const Cell & RegionHolder::GetNO() const
+{
+    return sNO_;
+}
+
+const Cell & RegionHolder::GetNE() const
+{
+    return sNE_;
+}
+
+const Cell & RegionHolder::GetN() const
+{
+    return sN_;
+}
+
+const Cell & RegionHolder::GetO() const
+{
+    return sO_;
+}
+
+const Cell & RegionHolder::GetC() const
+{
+    return sC_;
+}
+
+const Cell & RegionHolder::GetE() const
+{
+    return sE_;
+}
+
+const Cell & RegionHolder::GetS() const
+{
+    return sS_;
+}
+
+const Cell & RegionHolder::GetSO() const
+{
+    return sSO_;
+}
+
+const Cell & RegionHolder::GetSE() const
+{
+    return sSE_;
+}
--- Sudoku/Sudoku/RegionHolder.h
+++ Sudoku/Sudoku/RegionHolder.h
@@ -11,12 +11,23 @@
 
 #include "Region.h"
 
-class RegionHolder {
+class RegionHolder : public Region{
 protected:
-    Region sRegion_;
     
 public:
     RegionHolder(const Region & sRegion);
+    
+    virtual const Cell & GetNO() const;
+    virtual const Cell & GetNE() const;
+    virtual const Cell & GetN() const;
+    virtual const Cell & GetO() const;
+    virtual const Cell & GetC() const;
+    virtual const Cell & GetE() const;
+    virtual const Cell & GetS() const;
+    virtual const Cell & GetSO() const;
+    virtual const Cell & GetSE() const;
+
+    
 };
 
 #endif
