7bafe28ff6e0089bfb43eed660d631df6a112c0b
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-01-27 17:22:34 +0100

    DEV: Add definition of OnlyOneChoiceInRegionVisitor

--- Sudoku/Sudoku/OnlyOneChoiceInRegionVisitor.cpp
+++ Sudoku/Sudoku/OnlyOneChoiceInRegionVisitor.cpp
@@ -10,54 +10,63 @@
 #include "OnlyOneChoiceInRegionVisitor.h"
 #include "LastCellFinder.h"
 
-/*bool OnlyOneChoiceInRegionVisitor::Visit(const Grid &ioGrid) const
+bool OnlyOneChoiceInRegionVisitor::Visit(Grid &ioGrid) const
 {
     bool bResult = false;
     
     RegionHolder sNO = ioGrid.GetNO();
-    LastCellFinder sLastCellFinderNO(sNO.TopRow(), sNO.MiddleRow(), sNO.BottomRow());
+    RowHolder sNOTop = sNO.TopRow(), sNOMiddle = sNO.MiddleRow(), sNOBottom = sNO.BottomRow();
+    LastCellFinder sLastCellFinderNO(sNOTop, sNOMiddle, sNOBottom);
     bool bFill = sLastCellFinderNO.fill();
     bResult = bFill || bResult;
     
     RegionHolder sN = ioGrid.GetN();
-    LastCellFinder sLastCellFinderN(sN.TopRow(), sN.MiddleRow(), sN.BottomRow());
+    RowHolder sNTop = sN.TopRow(), sNMiddle = sN.MiddleRow(), sNBottom = sN.BottomRow();
+    LastCellFinder sLastCellFinderN(sNTop, sNMiddle, sNBottom);
     bFill = sLastCellFinderN.fill();
     bResult = bFill || bResult;
     
     RegionHolder sNE = ioGrid.GetNE();
-    LastCellFinder sLastCellFinderNE(sNE.TopRow(), sNE.MiddleRow(), sNE.BottomRow());
+    RowHolder sNETop = sNE.TopRow(), sNEMiddle = sNE.MiddleRow(), sNEBottom = sNE.BottomRow();
+    LastCellFinder sLastCellFinderNE(sNETop, sNEMiddle, sNEBottom);
     bFill = sLastCellFinderNE.fill();
     bResult = bFill || bResult;
     
     RegionHolder sO = ioGrid.GetO();
-    LastCellFinder sLastCellFinderO(sO.TopRow(), sO.MiddleRow(), sO.BottomRow());
+    RowHolder sOTop = sO.TopRow(), sOMiddle = sO.MiddleRow(), sOBottom = sO.BottomRow();
+    LastCellFinder sLastCellFinderO(sOTop, sOMiddle, sOBottom);
     bFill = sLastCellFinderO.fill();
     bResult = bFill || bResult;
     
     RegionHolder sC = ioGrid.GetC();
-    LastCellFinder sLastCellFinderC(sC.TopRow(), sC.MiddleRow(), sC.BottomRow());
+    RowHolder sCTop = sC.TopRow(), sCMiddle = sC.MiddleRow(), sCBottom = sC.BottomRow();
+    LastCellFinder sLastCellFinderC(sCTop, sCMiddle, sCBottom);
     bFill = sLastCellFinderC.fill();
     bResult = bFill || bResult;
     
     RegionHolder sE = ioGrid.GetE();
-    LastCellFinder sLastCellFinderE(sE.TopRow(), sE.MiddleRow(), sE.BottomRow());
+    RowHolder sETop = sE.TopRow(), sEMiddle = sE.MiddleRow(), sEBottom = sE.BottomRow();
+    LastCellFinder sLastCellFinderE(sETop, sEMiddle, sEBottom);
     bFill = sLastCellFinderE.fill();
     bResult = bFill || bResult;
     
     RegionHolder sSO = ioGrid.GetSO();
-    LastCellFinder sLastCellFinderSO(sSO.TopRow(), sSO.MiddleRow(), sSO.BottomRow());
+    RowHolder sSOTop = sSO.TopRow(), sSOMiddle = sSO.MiddleRow(), sSOBottom = sSO.BottomRow();
+    LastCellFinder sLastCellFinderSO(sSOTop, sSOMiddle, sSOBottom);
     bFill = sLastCellFinderSO.fill();
     bResult = bFill || bResult;
     
     RegionHolder sS = ioGrid.GetS();
-    LastCellFinder sLastCellFinderS(sS.TopRow(), sS.MiddleRow(), sS.BottomRow());
+    RowHolder sSTop = sS.TopRow(), sSMiddle = sS.MiddleRow(), sSBottom = sS.BottomRow();
+    LastCellFinder sLastCellFinderS(sSTop, sSMiddle, sSBottom);
     bFill = sLastCellFinderS.fill();
     bResult = bFill || bResult;
     
     RegionHolder sSE = ioGrid.GetSE();
-    LastCellFinder sLastCellFinderSE(sSE.TopRow(), sSE.MiddleRow(), sSE.BottomRow());
+    RowHolder sSETop = sSE.TopRow(), sSEMiddle = sSE.MiddleRow(), sSEBottom = sSE.BottomRow();
+    LastCellFinder sLastCellFinderSE(sSETop, sSEMiddle, sSEBottom);
     bFill = sLastCellFinderSE.fill();
     bResult = bFill || bResult;
     
     return bResult;
-}*/
+}
