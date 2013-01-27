313915a3f22b22a434680f75c846fdcbddc099af
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-01-27 18:01:30 +0100

    DEV: Fix construction value in OnlyOneChoiceInRowVisitor and OnlyOneChoiceInColumnVisitor

--- Sudoku/Sudoku/OnlyOneChoiceInColumnVisitor.cpp
+++ Sudoku/Sudoku/OnlyOneChoiceInColumnVisitor.cpp
@@ -17,73 +17,73 @@ bool OnlyOneChoiceInColumnVisitor::Visit(Grid &ioGrid) const
     bool bResult = false;
     
     //  First column
-    ColumnHolder    sColumnHolder11(ioGrid.GetNO().GetNO(), ioGrid.GetNO().GetO(), ioGrid.GetNO().GetSO()),
-                    sColumnHolder12(ioGrid.GetO().GetNO(), ioGrid.GetO().GetO(), ioGrid.GetO().GetSO()),
-                    sColumnHolder13(ioGrid.GetSO().GetNO(), ioGrid.GetSO().GetO(), ioGrid.GetSO().GetSO());
+    ColumnHolder    sColumnHolder11 = ioGrid.GetNO().LeftColumn(),
+                    sColumnHolder12 = ioGrid.GetO().LeftColumn(),
+                    sColumnHolder13 = ioGrid.GetSO().LeftColumn();
     LastCellFinder sLastCellFinder1(sColumnHolder11, sColumnHolder12, sColumnHolder13);
     bool bFill = sLastCellFinder1.fill();
     bResult = bFill || bResult;
     
     //  Second column
-    ColumnHolder    sColumnHolder21(ioGrid.GetNO().GetN(), ioGrid.GetNO().GetC(), ioGrid.GetNO().GetS()),
-                    sColumnHolder22(ioGrid.GetO().GetN(), ioGrid.GetO().GetC(), ioGrid.GetO().GetS()),
-                    sColumnHolder23(ioGrid.GetSO().GetN(), ioGrid.GetSO().GetC(), ioGrid.GetSO().GetS());
+    ColumnHolder    sColumnHolder21 = ioGrid.GetNO().MiddleColumn(),
+                    sColumnHolder22 = ioGrid.GetO().MiddleColumn(),
+                    sColumnHolder23 = ioGrid.GetSO().MiddleColumn();
     LastCellFinder sLastCellFinder2(sColumnHolder21, sColumnHolder22, sColumnHolder23);
     bFill = sLastCellFinder2.fill();
     bResult = bFill || bResult;
     
     //  Third column
-    ColumnHolder    sColumnHolder31(ioGrid.GetNO().GetNE(), ioGrid.GetNO().GetE(), ioGrid.GetNO().GetSE()),
-                    sColumnHolder32(ioGrid.GetO().GetNE(), ioGrid.GetO().GetE(), ioGrid.GetO().GetSE()),
-                    sColumnHolder33(ioGrid.GetSO().GetNE(), ioGrid.GetSO().GetE(), ioGrid.GetSO().GetSE());
+    ColumnHolder    sColumnHolder31 = ioGrid.GetNO().RightColumn(),
+                    sColumnHolder32 = ioGrid.GetO().RightColumn(),
+                    sColumnHolder33 = ioGrid.GetSO().RightColumn();
     LastCellFinder sLastCellFinder3(sColumnHolder31, sColumnHolder32, sColumnHolder33);
     bFill = sLastCellFinder3.fill();
     bResult = bFill || bResult;
     
     //  Fourth column
-    ColumnHolder    sColumnHolder41(ioGrid.GetN().GetNO(), ioGrid.GetN().GetO(), ioGrid.GetN().GetSO()),
-                    sColumnHolder42(ioGrid.GetC().GetNO(), ioGrid.GetC().GetO(), ioGrid.GetC().GetSO()),
-                    sColumnHolder43(ioGrid.GetS().GetNO(), ioGrid.GetS().GetO(), ioGrid.GetS().GetSO());
+    ColumnHolder    sColumnHolder41 = ioGrid.GetN().LeftColumn(),
+                    sColumnHolder42 = ioGrid.GetC().LeftColumn(),
+                    sColumnHolder43 = ioGrid.GetS().LeftColumn();
     LastCellFinder sLastCellFinder4(sColumnHolder41, sColumnHolder42, sColumnHolder43);
     bFill = sLastCellFinder4.fill();
     bResult = bFill || bResult;
     
     //  Fifth column
-    ColumnHolder    sColumnHolder51(ioGrid.GetN().GetN(), ioGrid.GetN().GetC(), ioGrid.GetN().GetS()),
-                    sColumnHolder52(ioGrid.GetC().GetN(), ioGrid.GetC().GetC(), ioGrid.GetC().GetS()),
-                    sColumnHolder53(ioGrid.GetS().GetN(), ioGrid.GetS().GetC(), ioGrid.GetS().GetS());
+    ColumnHolder    sColumnHolder51 = ioGrid.GetN().MiddleColumn(),
+                    sColumnHolder52 = ioGrid.GetC().MiddleColumn(),
+                    sColumnHolder53 = ioGrid.GetS().MiddleColumn();
     LastCellFinder sLastCellFinder5(sColumnHolder51, sColumnHolder52, sColumnHolder53);
     bFill = sLastCellFinder5.fill();
     bResult = bFill || bResult;
     
     //  Sixth column
-    ColumnHolder    sColumnHolder61(ioGrid.GetN().GetNE(), ioGrid.GetN().GetE(), ioGrid.GetN().GetSE()),
-                    sColumnHolder62(ioGrid.GetC().GetNE(), ioGrid.GetC().GetE(), ioGrid.GetC().GetSE()),
-                    sColumnHolder63(ioGrid.GetS().GetNE(), ioGrid.GetS().GetE(), ioGrid.GetS().GetSE());
+    ColumnHolder    sColumnHolder61 = ioGrid.GetN().RightColumn(),
+                    sColumnHolder62 = ioGrid.GetC().RightColumn(),
+                    sColumnHolder63 = ioGrid.GetS().RightColumn();
     LastCellFinder sLastCellFinder6(sColumnHolder61, sColumnHolder62, sColumnHolder63);
     bFill = sLastCellFinder6.fill();
     bResult = bFill || bResult;
     
-    //  First column
-    ColumnHolder    sColumnHolder71(ioGrid.GetNE().GetNO(), ioGrid.GetNE().GetO(), ioGrid.GetNE().GetSO()),
-                    sColumnHolder72(ioGrid.GetE().GetNO(), ioGrid.GetE().GetO(), ioGrid.GetE().GetSO()),
-                    sColumnHolder73(ioGrid.GetSE().GetNO(), ioGrid.GetSE().GetO(), ioGrid.GetSE().GetSO());
+    //  Seventh column
+    ColumnHolder    sColumnHolder71 = ioGrid.GetNE().LeftColumn(),
+                    sColumnHolder72 = ioGrid.GetE().LeftColumn(),
+                    sColumnHolder73 = ioGrid.GetSE().LeftColumn();
     LastCellFinder sLastCellFinder7(sColumnHolder71, sColumnHolder72, sColumnHolder73);
     bFill = sLastCellFinder7.fill();
     bResult = bFill || bResult;
     
-    //  Second column
-    ColumnHolder    sColumnHolder81(ioGrid.GetNE().GetN(), ioGrid.GetNE().GetC(), ioGrid.GetNE().GetS()),
-                    sColumnHolder82(ioGrid.GetE().GetN(), ioGrid.GetE().GetC(), ioGrid.GetE().GetS()),
-                    sColumnHolder83(ioGrid.GetSE().GetN(), ioGrid.GetSE().GetC(), ioGrid.GetSE().GetS());
+    //  Eighth column
+    ColumnHolder    sColumnHolder81 = ioGrid.GetNE().MiddleColumn(),
+                    sColumnHolder82 = ioGrid.GetE().MiddleColumn(),
+                    sColumnHolder83 = ioGrid.GetSE().MiddleColumn();
     LastCellFinder sLastCellFinder8(sColumnHolder81, sColumnHolder82, sColumnHolder83);
     bFill = sLastCellFinder8.fill();
     bResult = bFill || bResult;
     
-    //  Third column
-    ColumnHolder    sColumnHolder91(ioGrid.GetNE().GetNE(), ioGrid.GetNE().GetE(), ioGrid.GetNE().GetSE()),
-                    sColumnHolder92(ioGrid.GetE().GetNE(), ioGrid.GetE().GetE(), ioGrid.GetE().GetSE()),
-                    sColumnHolder93(ioGrid.GetSE().GetNE(), ioGrid.GetSE().GetE(), ioGrid.GetSE().GetSE());
+    //  Nineth column
+    ColumnHolder    sColumnHolder91 = ioGrid.GetNE().RightColumn(),
+                    sColumnHolder92 = ioGrid.GetE().RightColumn(),
+                    sColumnHolder93 = ioGrid.GetSE().RightColumn(); 
     LastCellFinder sLastCellFinder9(sColumnHolder91, sColumnHolder92, sColumnHolder93);
     bFill = sLastCellFinder9.fill();
     bResult = bFill || bResult;
--- Sudoku/Sudoku/OnlyOneChoiceInRowVisitor.cpp
+++ Sudoku/Sudoku/OnlyOneChoiceInRowVisitor.cpp
@@ -16,73 +16,73 @@ bool OnlyOneChoiceInRowVisitor::Visit(Grid &ioGrid) const
 {
     bool bResult = false;
     //  First line
-    RowHolder   sRowHolder11(ioGrid.GetNO().GetNO(), ioGrid.GetNO().GetN(), ioGrid.GetNO().GetNE()),
-                sRowHolder12(ioGrid.GetN().GetNO(), ioGrid.GetN().GetN(), ioGrid.GetN().GetNE()),
-                sRowHolder13(ioGrid.GetNE().GetNO(), ioGrid.GetNE().GetN(), ioGrid.GetNE().GetNE());
+    RowHolder   sRowHolder11 = ioGrid.GetNO().TopRow(),
+                sRowHolder12 = ioGrid.GetN().TopRow(),
+                sRowHolder13 = ioGrid.GetNE().TopRow();
     LastCellFinder sLastCellFinder1(sRowHolder11, sRowHolder12, sRowHolder13);
     bool bFill = sLastCellFinder1.fill();
     bResult = bResult || bFill;
     
     //  Second Line
-    RowHolder   sRowHolder21(ioGrid.GetNO().GetO(), ioGrid.GetNO().GetC(), ioGrid.GetNO().GetE()),
-                sRowHolder22(ioGrid.GetN().GetO(), ioGrid.GetN().GetC(), ioGrid.GetN().GetE()),
-                sRowHolder23(ioGrid.GetNE().GetO(), ioGrid.GetNE().GetC(), ioGrid.GetNE().GetE());
+    RowHolder   sRowHolder21 = ioGrid.GetNO().MiddleRow(),
+                sRowHolder22 = ioGrid.GetN().MiddleRow(),
+                sRowHolder23 = ioGrid.GetNE().MiddleRow();
     LastCellFinder sLastCellFinder2(sRowHolder21, sRowHolder22, sRowHolder23);
     bFill = sLastCellFinder2.fill();
     bResult = bResult || bFill;
     
     //  Third line
-    RowHolder   sRowHolder31(ioGrid.GetNO().GetSO(), ioGrid.GetNO().GetS(), ioGrid.GetNO().GetSE()),
-                sRowHolder32(ioGrid.GetN().GetSO(), ioGrid.GetN().GetS(), ioGrid.GetN().GetSE()),
-                sRowHolder33(ioGrid.GetNE().GetSO(), ioGrid.GetNE().GetS(), ioGrid.GetNE().GetSE());
+    RowHolder   sRowHolder31 = ioGrid.GetNO().BottomRow(),
+                sRowHolder32 = ioGrid.GetN().BottomRow(),
+                sRowHolder33 = ioGrid.GetNE().BottomRow(); 
     LastCellFinder sLastCellFinder3(sRowHolder31, sRowHolder32, sRowHolder33);
     bFill = sLastCellFinder3.fill();
     bResult = bResult || bFill;
     
     //  Fourth line
-    RowHolder   sRowHolder41(ioGrid.GetO().GetNO(), ioGrid.GetO().GetN(), ioGrid.GetO().GetNE()),
-                sRowHolder42(ioGrid.GetC().GetNO(), ioGrid.GetC().GetN(), ioGrid.GetC().GetNE()),
-                sRowHolder43(ioGrid.GetE().GetNO(), ioGrid.GetE().GetN(), ioGrid.GetE().GetNE());
+    RowHolder   sRowHolder41 = ioGrid.GetO().TopRow(),
+                sRowHolder42 = ioGrid.GetC().TopRow(),
+                sRowHolder43 = ioGrid.GetE().TopRow();
     LastCellFinder sLastCellFinder4(sRowHolder41, sRowHolder42, sRowHolder43);
     bFill = sLastCellFinder4.fill();
     bResult = bResult || bFill;
     
     //  Fifth Line
-    RowHolder   sRowHolder51(ioGrid.GetO().GetO(), ioGrid.GetO().GetC(), ioGrid.GetO().GetE()),
-                sRowHolder52(ioGrid.GetC().GetO(), ioGrid.GetC().GetC(), ioGrid.GetC().GetE()),
-                sRowHolder53(ioGrid.GetE().GetO(), ioGrid.GetE().GetC(), ioGrid.GetE().GetE());
+    RowHolder   sRowHolder51 = ioGrid.GetO().MiddleRow(),
+                sRowHolder52 = ioGrid.GetC().MiddleRow(),
+                sRowHolder53 = ioGrid.GetE().MiddleRow();
     LastCellFinder sLastCellFinder5(sRowHolder51, sRowHolder52, sRowHolder53);
     bFill = sLastCellFinder2.fill();
     bResult = bResult || bFill;
     
     //  Sixth line
-    RowHolder   sRowHolder61(ioGrid.GetO().GetSO(), ioGrid.GetO().GetS(), ioGrid.GetO().GetSE()),
-                sRowHolder62(ioGrid.GetC().GetSO(), ioGrid.GetC().GetS(), ioGrid.GetC().GetSE()),
-                sRowHolder63(ioGrid.GetE().GetSO(), ioGrid.GetE().GetS(), ioGrid.GetE().GetSE());
+    RowHolder   sRowHolder61 = ioGrid.GetO().BottomRow(),
+                sRowHolder62 = ioGrid.GetC().BottomRow(),
+                sRowHolder63 = ioGrid.GetE().BottomRow(); 
     LastCellFinder sLastCellFinder6(sRowHolder61, sRowHolder62, sRowHolder63);
     bFill = sLastCellFinder3.fill();
     bResult = bResult || bFill;
     
     //  Seventh line
-    RowHolder   sRowHolder71(ioGrid.GetSO().GetNO(), ioGrid.GetSO().GetN(), ioGrid.GetSO().GetNE()),
-                sRowHolder72(ioGrid.GetS().GetNO(), ioGrid.GetS().GetN(), ioGrid.GetS().GetNE()),
-                sRowHolder73(ioGrid.GetSE().GetNO(), ioGrid.GetSE().GetN(), ioGrid.GetSE().GetNE());
+    RowHolder   sRowHolder71 = ioGrid.GetSO().TopRow(),
+                sRowHolder72 = ioGrid.GetS().TopRow(),
+                sRowHolder73 = ioGrid.GetSE().TopRow();
     LastCellFinder sLastCellFinder7(sRowHolder71, sRowHolder72, sRowHolder73);
     bFill = sLastCellFinder1.fill();
     bResult = bResult || bFill;
     
     //  Eighth Line
-    RowHolder   sRowHolder81(ioGrid.GetSO().GetO(), ioGrid.GetSO().GetC(), ioGrid.GetSO().GetE()),
-                sRowHolder82(ioGrid.GetS().GetO(), ioGrid.GetS().GetC(), ioGrid.GetS().GetE()),
-                sRowHolder83(ioGrid.GetSE().GetO(), ioGrid.GetSE().GetC(), ioGrid.GetSE().GetE());
+    RowHolder   sRowHolder81 = ioGrid.GetSO().MiddleRow(),
+                sRowHolder82 = ioGrid.GetS().MiddleRow(),
+                sRowHolder83 = ioGrid.GetSE().MiddleRow();
     LastCellFinder sLastCellFinder8(sRowHolder81, sRowHolder82, sRowHolder83);
     bFill = sLastCellFinder2.fill();
     bResult = bResult || bFill;
     
     //  Nineth line
-    RowHolder   sRowHolder91(ioGrid.GetSO().GetSO(), ioGrid.GetSO().GetS(), ioGrid.GetSO().GetSE()),
-                sRowHolder92(ioGrid.GetS().GetSO(), ioGrid.GetS().GetS(), ioGrid.GetS().GetSE()),
-                sRowHolder93(ioGrid.GetSE().GetSO(), ioGrid.GetSE().GetS(), ioGrid.GetSE().GetSE());
+    RowHolder   sRowHolder91 = ioGrid.GetSO().BottomRow(),
+                sRowHolder92 = ioGrid.GetS().BottomRow(),
+                sRowHolder93 = ioGrid.GetSE().BottomRow(); 
     LastCellFinder sLastCellFinder9(sRowHolder91, sRowHolder92, sRowHolder93);
     bFill = sLastCellFinder3.fill();
     bResult = bResult || bFill;
