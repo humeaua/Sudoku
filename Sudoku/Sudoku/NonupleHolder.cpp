179de4328ee3c79ad284f7c8804bf0bb3db625cc
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2013-02-17 15:51:09 +0100

    DEV: Beginning of implementation of nonuple holder

---
+++ Sudoku/Sudoku/NonupleHolder.cpp
@@ -0,0 +1,60 @@
+//
+//  NonupleHolder.cpp
+//  Sudoku
+//
+//  Created by Alexandre HUMEAU on 17/02/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include "NonupleHolder.h"
+
+NonupleHolder::NonupleHolder(TripleHolder & s1, TripleHolder & s2, TripleHolder & s3) : s1_(s1.GetLeft()), s2_(s1.GetCenter()), s3_(s1.GetRight()),s4_(s1.GetLeft()), s5_(s1.GetCenter()), s6_(s1.GetRight()),s7_(s1.GetLeft()), s8_(s1.GetCenter()), s9_(s1.GetRight())
+{}
+
+NonupleHolder::NonupleHolder(Cell &sCell1, Cell & sCell2, Cell & sCell3, Cell & sCell4, Cell & sCell5, Cell & sCell6, Cell & sCell7, Cell & sCell8, Cell & sCell9) : s1_(sCell1), s2_(sCell2), s3_(sCell3), s4_(sCell4), s5_(sCell5), s6_(sCell6), s7_(sCell7), s8_(sCell8), s9_(sCell9)
+{}
+
+Cell & NonupleHolder::Get1()
+{
+    return s1_;
+}
+
+Cell & NonupleHolder::Get2()
+{
+    return s2_;
+}
+
+Cell & NonupleHolder::Get3()
+{
+    return s3_;
+}
+
+Cell & NonupleHolder::Get4()
+{
+    return s4_;
+}
+
+Cell & NonupleHolder::Get5()
+{
+    return s5_;
+}
+
+Cell & NonupleHolder::Get6()
+{
+    return s6_;
+}
+Cell & NonupleHolder::Get7()
+{
+    return s7_;
+}
+
+Cell & NonupleHolder::Get8()
+{
+    return s8_;
+}
+
+Cell & NonupleHolder::Get9()
+{
+    return s9_;
+}
---
+++ Sudoku/Sudoku/NonupleHolder.h
@@ -0,0 +1,31 @@
+//
+//  NonupleHolder.h
+//  Sudoku
+//
+//  Created by Alexandre HUMEAU on 17/02/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Sudoku_NonupleHolder_h
+#define Sudoku_NonupleHolder_h
+
+#include "TripleHolder.h"
+
+class NonupleHolder {
+    Cell &s1_, &s2_, &s3_, &s4_, &s5_, &s6_, &s7_, &s8_, &s9_;
+public:
+    NonupleHolder(TripleHolder & s1, TripleHolder & s2, TripleHolder & s3);
+    NonupleHolder(Cell &sCell1, Cell & sCell2, Cell & sCell3, Cell & sCell4, Cell & sCell5, Cell & sCell6, Cell & sCell7, Cell & sCell8, Cell & sCell9);
+    
+    virtual Cell & Get1();
+    virtual Cell & Get2();
+    virtual Cell & Get3();
+    virtual Cell & Get4();
+    virtual Cell & Get5();
+    virtual Cell & Get6();
+    virtual Cell & Get7();
+    virtual Cell & Get8();
+    virtual Cell & Get9();
+};
+
+#endif
